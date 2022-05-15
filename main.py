1cb89802bc698b9821cc136a17ed7954b8eca74a
LuisMonge <l.mongebolanos@gruppohera.it>
2022-05-15 16:44:18 +0200

    Clean code

--- main.py
+++ main.py
@@ -2,12 +2,10 @@
 # -*- coding: utf-8 -*-
 from utils.visualize import visualizeChamberHum, visualizeChamberOxi, visualizeChamberTemp
 from utils.clean import clean_data
-import dataset_review # all functions
 import pandas as pd
 import os
 import argparse
 
-# TODO argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_csv", type=str, required=True, help="path to csv")
 parser.add_argument("--metodo", type=str, required=True, help='Metodo empleado en la compostera (string).')
@@ -19,13 +17,13 @@ def main(input_csv, metodo):
     print('\t This will clean and visualize data.')
     
     print('2. Cleaning data \n')
-    input_csv_path = os.path.join(f'{os.getcwd()}\\Data\\{args.input_csv}')
+    input_csv_path = os.path.join(f'{os.getcwd()}\\data\\{args.input_csv}')
     clean_data(input_csv, metodo)
     # cleaned_data = pd.read_csv('C:\\Users\\L.MONGEBOLANOS\\OneDrive - Gruppo HERA\\Desktop\\Compost\\Data\\cleaned_data_abril.csv', sep=',', index_col=0)
 
     print('3. Reading cleaned_data')
     file_noext = input_csv.split('.')[0]
-    cleaned_data = pd.read_csv(f'./Data/{file_noext}_cleaned.csv', sep=',', index_col=0)
+    cleaned_data = pd.read_csv(f'./data/{file_noext}_cleaned.csv', sep=',', index_col=0)
     # cleaned_data = pd.read_csv(input_csv_path, sep=',', index_col=0)
     cleaned_data['Datetime'] = pd.to_datetime(cleaned_data['Datetime'])
     print(cleaned_data.head())
@@ -37,7 +35,7 @@ def main(input_csv, metodo):
     chamber2 = cleaned_data[cleaned_data['Chamber'] == 2]
     print(f'Muestra: \n {chamber0.head(2)} \n \n Chamber 1: \n {chamber1.head(2)} \n \n Chamber 2: \n {chamber2.head(2)} \n \n')
 
-    graph_path = os.path.join(f'{os.getcwd()}\\Graphs')
+    graph_path = os.path.join(f'{os.getcwd()}\\graphs')
     review_path = os.path.join(f'{os.getcwd()}\\ComposteraReviewed')
 
     # TODO CALL DATASET_REVIEW
